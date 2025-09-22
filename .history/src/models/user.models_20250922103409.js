fc27acd9288444d26398c9258f90a8b492b2b785
mertfurkandgn <mertfurkandgn1912@gmail.com>
2025-09-22 12:39:28 +0300

    add all db models

---
+++ .history/src/models/user.models_20250922103409.js
@@ -0,0 +1,46 @@
+import mongoose,{Schema} from "mongoose";
+
+const userSchema = new Schema(
+    {
+        username:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true,
+            index:true
+        },
+        email:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true,
+        },
+        fullname:{
+            type:String,
+            required:true,
+            trim:true,
+            index:true
+        },
+        avatar:{
+            type:String,
+            required:true
+        },
+        coverImage:{
+            type:String,
+        },
+        watchHistory:{
+            type:Schema.Types.ObjectId,
+            ref:"Video"
+        },
+        password:{
+            type:String,
+            required:[true,"password is required"],
+            minlength:[6,"Password must be at least 6 characters"]
+        },
+        refreshToken:{
+            type:String
+        }
+    }
+)
